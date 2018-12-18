67b0a7507c1d3d9018316c4908c4181abbe849d9
Andrey <umeca90@gmail.com>
2018-12-18 18:22:26 +0000

    Destroying StaticPages controller, add trailing newline, fixing variable for app version

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  root   'static_pages#home'
-  get  '/about',            to: 'static_pages#about'
-  get  '/about_author',     to: 'static_pages#about_author'
+
 end
