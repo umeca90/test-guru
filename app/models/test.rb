c71de0774e4e5f877443063b34d07b2a8bf5fc57
Andrey <umeca90@gmail.com>
2018-12-24 17:43:29 +0000

    finishing task for lesson ActiveRecord in action

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Test < ApplicationRecord
   belongs_to :category
   def self.titles_by_category(category_title)
-    joins(:category).where(categories: {title: category_title}).order(title: :DESC).pluck(:title)
+    joins(:category).where(categories: { title: category_title }).order(title: :DESC).pluck(:title)
   end
 end
