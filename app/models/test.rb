2c9705a3027ec26421e20b7be32390a90eac9c6c
Andrey <umeca90@gmail.com>
2019-01-11 09:06:27 +0000

    pushing task for lesson 'Model Associations'

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,12 @@
 
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :questions, dependent: :destroy
+  has_many :course_passages, dependent: :destroy
+  has_many :users, through: :course_passages
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+
+
   def self.titles_by_category(category_title)
     joins(:category).where(categories: { title: category_title }).order(title: :desc).pluck(:title)
   end
