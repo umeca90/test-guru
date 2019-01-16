4c5efbccf9799fca5fc8213fc39a6244751e3c0e
Andrey <umeca90@gmail.com>
2019-01-16 10:40:07 +0000

    Small fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -42,7 +42,7 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:body, :test_id)
+    params.require(:question).permit(:body)
   end
 
   def rescue_with_question_not_found
